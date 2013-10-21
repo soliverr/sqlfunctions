99796bc1e392abe31730a2b684ce7d189e7f87fc
sOliverr <soliverr@gmail.com>
2013-10-21 14:50:05 +0600

    Test for installation process

--- test_build.sh
+++ test_build.sh
@@ -2,16 +2,31 @@
 
 ./build.sh
 
-destdir=test
-
 package=`cat ./configure.ac | sed -ne 's/^AC_INIT(\([^,]*\)\s*,.*/\1/gp'`
 
-rm -rf $destdir
+#
+# Set up to test scripts locally
+#
 
-# Configure for local path to test scripts
-./configure --prefix=`pwd`/$destdir/usr --localstatedir=`pwd`/$destdir/var \
-            --sysconfdir=`pwd`/$destdir/etc --datadir=`pwd`/$destdir/share/$package \
-            --with-liblsb=`pwd`/$destdir/lib/lsb
+destdir=`pwd`/test
+
+rm -rf $destdir 2>&-
+
+./configure --prefix=$destdir/usr --localstatedir=$destdir/var \
+            --sysconfdir=$destdir/etc --datadir=$destdir/share/$package \
+            --with-liblsb=$destdir/lib/lsb
 
 make install || exit 1
 
+
+#
+# Set up to emulate system installation process
+#
+
+destdir=inst
+
+rm -rf $destdir 2>&-
+
+./configure
+
+make install DESTDIR=$destdir || exit 1
