e3aedf4c787634c992c0e4345eb6482ee51f59e8
sOliverr <soliverr@gmail.com>
2013-10-23 16:32:15 +0600

    * Add global SPOOL_DIR variables * Use ORADBA_LOG_FILE variable to write logs * Disable clean jobs in crontab by default * Run clean jobs as `oracle' user

--- test_build.sh
+++ test_build.sh
@@ -5,8 +5,32 @@
 package=`cat ./configure.ac | sed -ne 's/^AC_INIT(\([^,]*\)\s*,.*/\1/gp'`
 
 #
+#
+# Set up to emulate system installation process
+#
+
+echo
+echo Test installation ...
+echo
+
+destdir=inst
+
+rm -rf $destdir 2>&-
+
+./configure --with-confdir=/etc/oracle/oradba \
+            --with-logdir=/var/log/oracle/oradba-exec-sql \
+            --with-archivelogdir=/var/log/oracle/archive/oradba-exec-sql \
+            --with-spooldir=/var/spool/oracle/oradba-exec-sql 
+
+make install DESTDIR=$destdir || exit 1
+
+#
 # Set up to test scripts locally
 #
+
+echo
+echo Setup test units ...
+echo
 
 destdir=`pwd`/test
 
@@ -14,19 +38,19 @@ rm -rf $destdir 2>&-
 
 ./configure --prefix=$destdir/usr --localstatedir=$destdir/var \
             --sysconfdir=$destdir/etc --datadir=$destdir/share/$package \
-            --with-liblsb=$destdir/lib/lsb
+            --with-liblsb=$destdir/lib/lsb --with-confdir=$destdir/etc/oracle/oradba \
+            --with-logdir=$destdir/var/log/oracle/oradba-exec-sql \
+            --with-archivelogdir=$destdir/var/log/oracle/archive/oradba-exec-sql \
+            --with-spooldir=$destdir/var/spool/oracle/oradba-exec-sql
 
 make install || exit 1
 
+# Copy function libraries
+install -D ../oracle-base/orabase-functions $destdir/lib/lsb/orabase-functions
 
-#
-# Set up to emulate system installation process
-#
+# Run tests
+echo
+echo Run tests ...
+echo
 
-destdir=inst
 
-rm -rf $destdir 2>&-
-
-./configure
-
-make install DESTDIR=$destdir || exit 1
